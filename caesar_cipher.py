c72207bc6aab973493c6b5a293bda778a7cacd06
skoriy <skoriy88@gmail.com>
2018-05-24 18:21:32 +0300

    24.05

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -2,20 +2,38 @@
 0 < text < 50
 -26 < delta < 26
 '''
-inp = "important text"
+inp = "simple text"
 alpha = 'abcdefghijklmnopqrstuvwxyz'
-print(len(alpha))
 
 def to_encrypt(text, delta):
     res = []
-    for i in range(0, len(text)-1):
+    for i in range(0, len(text)):
         if text[i] == ' ':
             res.append(' ')
         else:
-            if alpha.find(text[i]) + delta < len(alpha) - 1:
-                print(alpha[alpha.find(text[i]) + delta])
+            if 0 <= alpha.find(text[i]) + delta <=  len(alpha) - 1:
                 res.append(alpha[alpha.find(text[i]) + delta])
+            elif alpha.find(text[i]) + delta > len(alpha) -1:
+                res.append(alpha[alpha.find(text[i]) + delta - len(alpha)])
+            else:
+               res.append(alpha[alpha.find(text[i]) + delta + len(alpha)])
+    res = ''.join(res)
     print(res)
     return res
 
-to_encrypt(inp, 3)
+to_encrypt(inp, 16)
+to_encrypt("a b c", -3)
+
+def to_encrypt(text, delta):
+    return ''.join([chr(ord('a') + (ord(i) + delta - ord('a')) % 26) if i != ' ' else ' ' for i in text ])
+
+def to_encrypt(text, delta):
+    return ''.join(chr(ord('a') + (ord(c) - ord('a') + delta) % 26)
+                   if c != ' ' else ' ' for c in text)
+
+to_encrypt=lambda t,k:''.join([x,chr(97+(ord(x)-97+k)%26)]['`'<x<'{']for x in t)
+
+def to_encrypt(text, delta):
+    f = lambda c: chr((ord(c) + 7 + delta) % 26 + 97) if c.isalpha() else c
+    return ''.join(map(f, text))
+
